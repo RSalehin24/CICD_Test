18e82d1622b942f69060104c4b1ec6b4c9676ced
Md. Reyanus Salehin <reyanussalehin@iut-dhaka.edu>
2021-12-01 11:44:34 +0600

    code added

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Azure')
+});
+
+app.listen(8080, () => {
+    console.log('Server is listening to the port 8080 ...')
+})
